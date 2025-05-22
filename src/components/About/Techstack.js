816ed3366fdc9dc2482dc4251e5f04ec0b3a8c66
kashyap1807 <kashyapchudasama0@gmail.com>
2025-05-22 23:02:00 +0530

    changes icons of tools stacks

--- src/components/About/Techstack.js
+++ src/components/About/Techstack.js
@@ -34,10 +34,73 @@ function Techstack() {
   return (
     <Row style={{ justifyContent: "center", paddingBottom: "50px" }}>
       <Col xs={4} md={2} className="tech-icons">
-        <SiCsharp />
+        <img
+          src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/csharp/csharp-original.svg"
+          height="80"
+          alt="csharp logo"
+        />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <img
+          src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/dotnetcore/dotnetcore-original.svg"
+          height="80"
+          alt="dotnetcore logo"
+        />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <img
+          src="https://cdn.simpleicons.org/angular/DD0031"
+          height="80"
+          alt="angularjs logo"
+        />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <img
+          src="https://cdn.simpleicons.org/typescript/3178C6"
+          height="80"
+          alt="typescript logo"
+        />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <img
+          src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/azure/azure-original.svg"
+          height="80"
+          alt="azure logo"
+        />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <a
+          href="https://learn.microsoft.com/en-us/sql/ssms/sql-server-management-studio-ssms"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <img
+            src="https://www.edureka.co/blog/wp-content/uploads/2019/10/logo.png"
+            alt="sql server management studio"
+            width="80"
+            height="80"
+          />
+        </a>
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <img
+          src="https://cdn.simpleicons.org/html5/E34F26"
+          height="80"
+          alt="html5 logo"
+        />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <img
+          src="https://cdn.simpleicons.org/css3/1572B6"
+          height="80"
+          alt="css3 logo"
+        />
       </Col>
 
-      <Col xs={4} md={2} className="tech-icons">
+      {/* <Col xs={4} md={2} className="tech-icons">
+        <SiCsharp />        
+      </Col> */}
+      {/* <Col xs={4} md={2} className="tech-icons">
         <DiDotnet />
       </Col>
       <Col xs={4} md={2} className="tech-icons">
@@ -46,10 +109,10 @@ function Techstack() {
       <Col xs={4} md={2} className="tech-icons">
         <SiTypescript />
       </Col>
-      
+
       <Col xs={4} md={2} className="tech-icons">
         <DiBootstrap />
-      </Col>
+      </Col> */}
     </Row>
   );
 }
--- src/components/About/Toolstack.js
+++ src/components/About/Toolstack.js
@@ -10,10 +10,64 @@ import {
 function Toolstack() {
   return (
     <Row style={{ justifyContent: "center", paddingBottom: "50px" }}>
-      {/* <Col xs={4} md={2} className="tech-icons">
-        <SiMacos />
-      </Col> */}
       <Col xs={4} md={2} className="tech-icons">
+        <img
+          src="https://cdn.simpleicons.org/bootstrap/7952B3"
+          height="80"
+          alt="bootstrap logo"
+        />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <img
+          src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/git/git-original.svg"
+          height="80"
+          alt="git logo"
+        />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <img
+          src="https://skillicons.dev/icons?i=github"
+          height="80"
+          alt="github logo"
+        />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <img
+          src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/npm/npm-original-wordmark.svg"
+          height="80"
+          alt="npm logo"
+        />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <img
+          src="https://cdn.simpleicons.org/nuget/004880"
+          height="80"
+          alt="nuget logo"
+        />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <img
+          src="https://cdn.simpleicons.org/postman/FF6C37"
+          height="80"
+          alt="postman logo"
+        />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <img
+          src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/visualstudio/visualstudio-plain.svg"
+          height="80"
+          alt="visualstudio logo"
+        />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <img
+          src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/vscode/vscode-original.svg"
+          height="80"
+          alt="vscode logo"
+        />
+      </Col>
+
+      {/* <Col xs={4} md={2} className="tech-icons">
         <SiVisualstudiocode />
       </Col>
       <Col xs={4} md={2} className="tech-icons">
@@ -24,7 +78,7 @@ function Toolstack() {
       </Col>
       <Col xs={4} md={2} className="tech-icons">
         <SiAzuredevops />
-      </Col>
+      </Col> */}
     </Row>
   );
 }
