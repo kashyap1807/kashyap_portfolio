129ff9fd6d59d38684e1f210e545d3d45a7eff57
kashyap1807 <kashyapchudasama0@gmail.com>
2025-07-31 23:41:19 +0530

    footer changes

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -16,10 +16,8 @@ function Footer() {
         <Col md="4" className="footer-copywright">
           <h3>Designed by Kashyap</h3>
         </Col>
-        <Col md="4" className="footer-copywright">
-          <h3>Copyright © {year} KC</h3>
-        </Col>
-        <Col md="4" className="footer-body">
+
+        {/* <Col md="4" className="footer-body">
           <ul className="footer-icons">
             <li className="social-icons">
               <a
@@ -51,17 +49,49 @@ function Footer() {
                 <FaLinkedinIn />
               </a>
             </li>
-            {/* <li className="social-icons">
-              <a
-                href="https://www.instagram.com/kashyap_1807_/"
-                style={{ color: "white" }}
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <AiFillInstagram />
-              </a>
-            </li> */}
+            
           </ul>
+        </Col> */}
+        <Col md="4" className="home-about-social">
+          <h2>FIND ME ON</h2>
+          <p>
+            Feel free to <span className="purple">connect </span>
+          </p>
+          <ul className="home-about-social-links">
+            <li className="social-icons">
+              <a
+                href="https://github.com/kashyap1807"
+                target="_blank"
+                rel="noreferrer"
+                className="icon-colour  home-social-icons"
+              >
+                <AiFillGithub />
+              </a>
+            </li>
+            <li className="social-icons">
+              <a
+                href="https://twitter.com/Kashyapp_01"
+                target="_blank"
+                rel="noreferrer"
+                className="icon-colour  home-social-icons"
+              >
+                <AiOutlineTwitter />
+              </a>
+            </li>
+            <li className="social-icons">
+              <a
+                href="https://www.linkedin.com/in/kashyap-chudasama-a17a6b238/"
+                target="_blank"
+                rel="noreferrer"
+                className="icon-colour  home-social-icons"
+              >
+                <FaLinkedinIn />
+              </a>
+            </li>
+          </ul>
+        </Col>
+        <Col md="4" className="footer-copywright">
+          <h3>Copyright © {year} KC</h3>
         </Col>
       </Row>
     </Container>
--- src/components/Home/Home2.js
+++ src/components/Home/Home2.js
@@ -48,46 +48,7 @@ function Home2() {
           </Col>
         </Row>
         <Github />
-        <Row>
-          <Col md={12} className="home-about-social">
-            <h1>FIND ME ON</h1>
-            <p>
-              Feel free to <span className="purple">connect </span>
-            </p>
-            <ul className="home-about-social-links">
-              <li className="social-icons">
-                <a
-                  href="https://github.com/kashyap1807"
-                  target="_blank"
-                  rel="noreferrer"
-                  className="icon-colour  home-social-icons"
-                >
-                  <AiFillGithub />
-                </a>
-              </li>
-              <li className="social-icons">
-                <a
-                  href="https://twitter.com/Kashyapp_01"
-                  target="_blank"
-                  rel="noreferrer"
-                  className="icon-colour  home-social-icons"
-                >
-                  <AiOutlineTwitter />
-                </a>
-              </li>
-              <li className="social-icons">
-                <a
-                  href="https://www.linkedin.com/in/kashyap-chudasama-a17a6b238/"
-                  target="_blank"
-                  rel="noreferrer"
-                  className="icon-colour  home-social-icons"
-                >
-                  <FaLinkedinIn />
-                </a>
-              </li>
-            </ul>
-          </Col>
-        </Row>
+        
       </Container>
     </Container>
   );
