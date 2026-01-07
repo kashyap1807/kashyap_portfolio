3dd70ecf1e00c9239a58815f6ffbaead851713eb
kashyap1807 <kashyapchudasama0@gmail.com>
2026-01-07 20:55:02 +0530

    Update footer text from "Designed by" to "Developed by" for consistency

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -14,7 +14,7 @@ function Footer() {
     <Container fluid className="footer">
       <Row>
         <Col md="4" className="footer-copywright">
-          <h3>Designed by Kashyap</h3>
+          <h3>Developed by Kashyap</h3>
         </Col>
 
         {/* <Col md="4" className="footer-body">
